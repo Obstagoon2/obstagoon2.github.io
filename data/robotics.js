dfb353893d7edb0c12343db43a17e3f6be72b712
Zakk Jackson <121466657+Obstagoon2@users.noreply.github.com>
2026-08-19 14:10:12 -0500

    Updated robotics roles

--- data/robotics.js
+++ data/robotics.js
@@ -1,41 +1,41 @@
 const roboticsRoles = [
   {
-    organization: "FRC 1912 Combustion",
+    organization: "FRC 1912 Combustion | 2026-27",
     program: "FRC",
     season: "2026–27",
-    roles: ["Vice Captain"],
+    roles: ["Vice Captain & FTC Program Captain"],
     current: true
   },
   {
-    organization: "FRC 1912 Combustion",
+    organization: "FRC 1912 Combustion | 2025-26",
     program: "FRC",
     season: "2025–26",
     roles: ["Vice Captain of Engineering", "Pit Captain", "Safety Captain"],
     current: false
   },
   {
-    organization: "FRC 1912 Combustion",
+    organization: "FRC 1912 Combustion | 2024-25",
     program: "FRC",
     season: "2024–25",
     roles: ["Drive Coach"],
     current: false
   },
   {
-    organization: "FTC 27406 Impulse",
+    organization: "FTC 27406 Impulse | 2025-26",
     program: "FTC",
     season: "2025–26",
     roles: ["Captain", "Drive Coach"],
     current: false
   },
   {
-    organization: "FTC 27406 Impulse",
+    organization: "FTC 27406 Impulse | 2024-25",
     program: "FTC",
     season: "2024–25",
     roles: ["Captain", "Driver"],
     current: false
   },
   {
-    organization: "FTC 13017 Event Horizon",
+    organization: "FTC 13017 Event Horizon | 2023-24",
     program: "FTC",
     season: "2023–24",
     roles: ["Programmer", "Driver at first competition"],
